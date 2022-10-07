e22e2665a8a331f3cf897b6fcffbf2adb3208b77
Francis <francisodhiambo2@gmail.com>
2022-10-07 13:12:10 +0300

    Done

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,3 +1,34 @@
+# class ToysController < ApplicationController
+#   wrap_parameters format: []
+
+#   def index
+#     toys = Toy.all
+#     render json: toys
+#   end
+
+#   def create
+#     toy = Toys.create(toy_params)
+#     render json: toy, status: :created
+#   end
+
+#   def update
+#     toy = Toy.find_by(id: params[:id])
+#     toy.update(toy_params)
+#   end
+
+#   def destroy
+#     toy = Toy.find_by(id: params[:id])
+#     toy.destroy
+#     head :no_content
+#   end
+
+#   private
+  
+#   def toy_params
+#     params.permit(:name, :image, :likes)
+#   end
+
+# end
 class ToysController < ApplicationController
   wrap_parameters format: []
 
@@ -7,13 +38,16 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create!(toy_params)
     render json: toy, status: :created
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
+    render json: toy, status: :accepted
   end
 
   def destroy
@@ -29,3 +63,5 @@ class ToysController < ApplicationController
   end
 
 end
+
+
